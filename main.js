aae02c12de17a0b76ccf5c555e2f79be994eb45e
angelnieto18 <angelnietomatamoros@gmail.com>
2023-01-26 19:25:55 -0400

    se ha agregado la funcion renderData que muestra en pantalla la imágen del pokemon

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const findPokemon = async (event) => {
-	event.preventDefault();
-	const data = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
-	const json = await data.json()
+	try {
+		event.preventDefault();
+		const {value} = event.target.pokemonName
+		const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${value}`);
+		const json = await data.json();
+		renderData(json);
+	} catch(e) {
+		alert("Ese pokemon no existe");
+	}
+}
+
+const card = document.getElementById("card");
+const avatar = document.getElementById("avatar");
+
+const renderData = (pokemon) => { 
+	avatar.setAttribute("src", `${pokemon.sprites.front_default}`)
+	avatar.setAttribute("alt", `${pokemon.name}`)
+	avatar.setAttribute("width", "200")
+	avatar.setAttribute("height", "200")
 }
