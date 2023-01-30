4bd0d71a48e8e7c20b3162076dd8732706590450
angelnieto18 <angelnietomatamoros@gmail.com>
2023-01-30 19:45:27 -0400

    se ha agregado una funcion para rendererizar los tipos de pokemon

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const card = document.getElementById("card");
+const avatar = document.getElementById("avatar");
+const typesContainer = document.getElementById("typesContainer");
+const typesList = document.getElementById("typesList");
+
 const findPokemon = async (event) => {
 	try {
 		event.preventDefault();
@@ -6,16 +11,24 @@ const findPokemon = async (event) => {
 		const json = await data.json();
 		renderData(json);
 	} catch(e) {
+		console.error(e)
 		alert("Ese pokemon no existe");
 	}
 }
 
-const card = document.getElementById("card");
-const avatar = document.getElementById("avatar");
-
 const renderData = (pokemon) => { 
-	avatar.setAttribute("src", `${pokemon.sprites.front_default}`)
+	avatar.setAttribute("src", `${pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default}`)
 	avatar.setAttribute("alt", `${pokemon.name}`)
-	avatar.setAttribute("width", "200")
-	avatar.setAttribute("height", "200")
+	avatar.setAttribute("width", "150")
+	avatar.setAttribute("height", "150")
+	renderTypes(pokemon.types)
+}
+
+const renderTypes = (pokemonTypes) => {
+	typesList.innerHTML = ''
+	for (types of pokemonTypes) {
+		const li = document.createElement("li")
+		li.textContent = types.type.name
+		typesList.append(li)
+	}
 }
