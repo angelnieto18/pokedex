17bfab8c04b09bbd53be2c8f92545e8da6fddfa0
angelnieto18 <angelnietomatamoros@gmail.com>
2023-05-25 18:59:02 -0400

    primeros pasos con una pokedex basada en React

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Pokedex from './components/Pokedex/Pokedex'
+import './App.css'
+
+export const App = () => {
+  return (
+    <Pokedex />
+  )
+}
+
+export default App
---
+++ src/components/Pokedex/Pokedex.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Pokedex = () => {
+  return (
+    <>
+    <div id='container'>
+
+    </div>
+    <div id='pokemonContainer'>
+
+    </div>
+    </>
+    )
+}
+
+export default Pokedex
